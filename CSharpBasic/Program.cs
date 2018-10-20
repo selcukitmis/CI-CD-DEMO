bceac5e519bd44b8f58f0758ad478803a1add926
selcukitmis <slck87@hotmail.com>
2018-10-21 02:56:36 +0300

    sayi

--- CSharpBasic/Program.cs
+++ CSharpBasic/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using HtmlAgilityPack;
 
 namespace CSharpBasic
 {
@@ -10,7 +12,34 @@ namespace CSharpBasic
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            var pg = GetPageCount();
+            Console.WriteLine(pg);
+            Console.ReadLine();
+        }
+
+        private static string GetContent(string urlAddress)
+        {
+            Uri url = new Uri(urlAddress);
+            WebClient client = new WebClient();
+            client.Encoding = System.Text.Encoding.UTF8;
+
+            string html = client.DownloadString(url);
+            return html;
+        }
+
+        private static int GetPageCount()
+        {
+            Uri url = new Uri("https://www.w3schools.com/");
+            WebClient client = new WebClient();
+            string html = client.DownloadString(url);
+            // Adresten istek yapı html kodlarını indiriyoruz.     
+
+            HtmlAgilityPack.HtmlDocument dokuman = new HtmlAgilityPack.HtmlDocument();
+            dokuman.LoadHtml(html);
+            // İndirdiğimiz html kodlarını bir HtmlDocment nesnesine yüklüyoruz.     
+            HtmlNodeCollection basliklar = dokuman.DocumentNode.SelectNodes("//a[@class='w3-bar-item w3-button']");
+            // Html dökümanı içndeki h2 etiketlerinden class'ı  tutheader olanları liste halinde alıyoruzç    
+            return basliklar.Count;
         }
     }
 }
